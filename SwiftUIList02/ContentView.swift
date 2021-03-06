0fdea42cdfc16df69e846773e0e5eed5ac7bf0c4
Mamoru Sugihara <sugiharam2@gmail.com>
2021-03-07 01:10:02 +0900

    Initial Commit

---
+++ SwiftUIList02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIList02
+//
+//  Created by Mamoru Sugihara on 2021/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIList02/SwiftUIList02App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIList02App.swift
+//  SwiftUIList02
+//
+//  Created by Mamoru Sugihara on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIList02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
