0d487616ed1b97d264221543299f975cc2b8e962
Mamoru Sugihara <sugiharam2@gmail.com>
2021-03-07 04:19:36 +0900

    Check rotation

--- SwiftUIList02/ContentView.swift
+++ SwiftUIList02/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+	@State var selected = 0
+
+	var rot: Angle {
+		get { return .degrees(Double(-selected * 10)) }
+	}
+
+	var body: some View {
+		GeometryReader { geo in
+			List(0..<100) { n in
+				ZStack {
+					Color.blue.opacity(0.3)
+					Text("Item:\(n)")
+				}
+				.contentShape(Rectangle())
+				.onAppear {
+					print(n)
+				}
+				.onTapGesture {
+					selected = n
+				}
+			}
+			.rotationEffect(rot, anchor: .topLeading)
+			.frame(width: geo.size.height, height: geo.size.width)
+			.animation(.default)
+		}
     }
 }
 
